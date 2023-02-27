2e420d00b6cc0b2b0062ecb72283ddec827afb79
bobokamura <roberto.okamura@cotefacil.com>
2023-02-27 19:41:28 -0300

    Lift State

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import "./App.css";
+import Saudacao from "./Saudacao";
+import SeuNome from "./SeuNome";
 
 function App() {
-
+  const [nome, setNome] = useState();
 
   return (
     <div className="App">
-      <h1>***</h1>
-      
+      <h1>Lift State</h1>
+      <SeuNome setNome={setNome} />
+      <Saudacao nome = {nome}/>
     </div>
   );
 }
